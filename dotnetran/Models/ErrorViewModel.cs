fcfac7c3937428d5557b1f5e49f0ead0a5be7a8b
larryw3i <larryw3i@163.com>
2020-11-15 10:38:49 +0800

    init

---
+++ dotnetran/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetran.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
